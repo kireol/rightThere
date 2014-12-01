2ba298d874dae391bb605dd87c9afc646bc9ade3
Chris Parent <christopher.parent@onstar.com>
2014-11-30 21:34:01 -0500

    CP code cleanup

--- rightThere.ino
+++ rightThere.ino
@@ -5,71 +5,39 @@
 #define GreenLEDPin 8
 #define TRIGGER_PIN  12
 #define ECHO_PIN     9
-#define MAX_DISTANCE 500
 
-
+//inches for minimum threshold
 #define GREEN_MIN_DISTANCE 40
 #define YELLOW_MIN_DISTANCE 30
 #define RED_MIN_DISTANCE 20
 
-boolean seenRed = false;
-int huns, tens, ones, distance, returnbyte, sonar_data, i;
-unsigned long time_start;
+#define MAX_DISTANCE 200
 
-void wake_up() {
-}
+boolean seenRed = false;
+int huns, tens, ones, distance, sonar_data, i;
+unsigned long time_start;
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 void showGreenLED();
-
 void showYellowLED();
-
 void showRedLED();
-
 void sleepModeHandler();
-
-void turnOffLEDs() {
-    digitalWrite(GreenLEDPin, LOW);
-    digitalWrite(RedLEDPin, LOW);
-    digitalWrite(YellowLEDPin, LOW);
-}
+void cycleGreenToRed();
 
 void setup() {
     Serial.begin(9600);
     pinMode(GreenLEDPin, OUTPUT);
     pinMode(RedLEDPin, OUTPUT);
     pinMode(YellowLEDPin, OUTPUT);
-    //   turnOffLEDs();
-}
-
-void cycleGreenToRed(int delaySeconds) {
-    digitalWrite(GreenLEDPin, HIGH);
-    delay(delaySeconds);
-    digitalWrite(GreenLEDPin, LOW);
-    digitalWrite(YellowLEDPin, HIGH);
-    delay(delaySeconds);
-    digitalWrite(YellowLEDPin, LOW);
-    digitalWrite(RedLEDPin, HIGH);
-    delay(delaySeconds);
-    digitalWrite(RedLEDPin, LOW);
-    delay(delaySeconds);
-}
-
-void go_to_sleep() {
-    delay(10000);
-    for (i = 0; i < 1; i++) {
-        cycleGreenToRed(50);
-    }
-    time_start = millis();
 }
 
 void loop() {
     delay(50);
     unsigned int distance = sonar.ping() / 100;
-    Serial.print("Ping: ");
-    Serial.print(distance);
-    Serial.println(" in");
+//    Serial.print("Ping: ");
+//    Serial.print(distance);
+//    Serial.println(" in");
 
     if (distance > GREEN_MIN_DISTANCE) {
         seenRed = false;
@@ -86,7 +54,38 @@ void loop() {
         cycleGreenToRed(50);
     }
 
-    //sleepModeHandler();
+    sleepModeHandler();
+}
+
+void turnOffLEDs() {
+    digitalWrite(GreenLEDPin, LOW);
+    digitalWrite(RedLEDPin, LOW);
+    digitalWrite(YellowLEDPin, LOW);
+}
+
+void cycleGreenToRed(int delaySeconds) {
+    digitalWrite(GreenLEDPin, HIGH);
+    delay(delaySeconds);
+    digitalWrite(GreenLEDPin, LOW);
+    digitalWrite(YellowLEDPin, HIGH);
+    delay(delaySeconds);
+    digitalWrite(YellowLEDPin, LOW);
+    digitalWrite(RedLEDPin, HIGH);
+    delay(delaySeconds);
+    digitalWrite(RedLEDPin, LOW);
+    delay(delaySeconds);
+}
+
+void wake_up() {
+    cycleGreenToRed(50);
+}
+
+void go_to_sleep() {
+    delay(10000);
+    for (i = 0; i < 1; i++) {
+        cycleGreenToRed(50);
+    }
+    time_start = millis();
 }
 
 void sleepModeHandler() {
@@ -116,4 +115,3 @@ void showGreenLED() {
     digitalWrite(RedLEDPin, LOW);
     digitalWrite(YellowLEDPin, LOW);
 }
-
