53ac899b0661a3a21e1eeb0b016c3fbeb09b9866
Chris Parent <chris@motorcitysoftware.com>
2015-12-31 20:29:04 -0500

    CP - added potentiometer

--- rightThere.ino
+++ rightThere.ino
@@ -6,24 +6,31 @@
 #define TRIGGER_PIN  13
 #define ECHO_PIN     11
 
-//inches for minimum threshold
-#define GREEN_MIN_DISTANCE 76
-#define YELLOW_MIN_DISTANCE 48
-#define RED_MIN_DISTANCE 29
+#define MAX_DISTANCE 300
 
-#define MAX_DISTANCE 200
+//inches for minimum threshold
+int RED_MIN_DISTANCE;
+int YELLOW_MIN_DISTANCE;
+int GREEN_MIN_DISTANCE;
+
 
 boolean seenRed = false;
 int huns, tens, ones, distance, sonar_data, i;
 unsigned long time_start;
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void showGreenLED();
+
 void showYellowLED();
+
 void showRedLED();
+
 void sleepModeHandler();
+
 void cycleGreenToRed();
+
+void setDistances(int minimum);
 
 void setup() {
     Serial.begin(9600);
@@ -33,34 +40,52 @@ void setup() {
 }
 
 void loop() {
-    delay(50);
+    int sensorValue = analogRead(A0);
+
     unsigned int distance = sonar.ping() / 100;
     turnOffLEDs();
 //    Serial.print("Ping: ");
 //    Serial.print(distance);
 //    Serial.println(" in");
+    int potentiometerDistance = analogRead(A0) / 10;
+    setDistances(potentiometerDistance);
+
     unsigned int GREEN_TIME_MULTIPLIER = 300 / (MAX_DISTANCE - GREEN_MIN_DISTANCE);
     unsigned int YELLOW_TIME_MULTIPLIER = 300 / (GREEN_MIN_DISTANCE - YELLOW_MIN_DISTANCE);
     unsigned int RED_TIME_MULTIPLIER = 500 / (YELLOW_MIN_DISTANCE - RED_MIN_DISTANCE);
+
+
+    Serial.print("distance: ");
+    Serial.print(distance);
+    Serial.println(" inches");
+    Serial.print("Red min: ");
+    Serial.println(RED_MIN_DISTANCE);
+    Serial.print("Yellow min: ");
+    Serial.println(YELLOW_MIN_DISTANCE);
+    Serial.print("Green min: ");
+    Serial.println(GREEN_MIN_DISTANCE);
+    Serial.println("");
     
     if (distance > GREEN_MIN_DISTANCE) {
         seenRed = false;
-        delay( (distance - GREEN_MIN_DISTANCE) * GREEN_TIME_MULTIPLIER);
+        delay((distance - GREEN_MIN_DISTANCE) * GREEN_TIME_MULTIPLIER);
         showGreenLED();
     }
     else if (distance > YELLOW_MIN_DISTANCE && distance <= GREEN_MIN_DISTANCE) {
         seenRed = false;
-        delay( (distance - YELLOW_MIN_DISTANCE) * YELLOW_TIME_MULTIPLIER);
+        delay((distance - YELLOW_MIN_DISTANCE) * YELLOW_TIME_MULTIPLIER);
         showYellowLED();
     }
     else if (distance <= YELLOW_MIN_DISTANCE && distance >= RED_MIN_DISTANCE) {
-        delay( (distance - RED_MIN_DISTANCE) * RED_TIME_MULTIPLIER);
+        delay((distance - RED_MIN_DISTANCE) * RED_TIME_MULTIPLIER);
         showRedLED();
     }
     else if (distance < RED_MIN_DISTANCE && distance > 0) {
         cycleGreenToRed(50);
     }
     sleepModeHandler();
+    delay(100);
+
 }
 
 void turnOffLEDs() {
@@ -98,13 +123,18 @@ void sleepModeHandler() {
     if (millis() - time_start < 0) // means we had a roll over since time_start is greater than millis
         time_start = 0;
 
-    if (millis() - time_start > 60000) {
+    if (millis() - time_start > 60000 || time_start == 0) {
         turnOffLEDs();
         go_to_sleep();
     }
 }
 
+void setDistances(int minimum) {
+    RED_MIN_DISTANCE = 2 + minimum;
+    YELLOW_MIN_DISTANCE = RED_MIN_DISTANCE + 30;
+    GREEN_MIN_DISTANCE = YELLOW_MIN_DISTANCE + 30;
 
+}
 
 void showRedLED() {
     digitalWrite(RedLEDPin, HIGH);
